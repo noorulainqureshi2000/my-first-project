bb82a88833b51d8cd7f7e421a9ac4fcee11bd389
noorulainqureshi2000 <noorulainqureshi2000@gmail.com>
2026-02-08 15:20:57 +0500

    Add files via upload

---
+++ assigment#3.py
@@ -0,0 +1,15 @@
+# Area of Circle
+radius = 10
+area_circle = 3.14 * radius * radius
+print("Area of circle:", round(area_circle, 2))
+
+# Area of Triangle
+base = 10
+height = 5
+area_triangle = 0.5 * base * height
+print("Area of triangle:", round(area_triangle, 2))
+
+# Area of Square
+side = 4
+area_square = side * side
+print("Area of square:", area_square)
